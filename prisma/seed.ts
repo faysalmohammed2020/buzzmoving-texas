87c9dce8b9446adc1313cc7af091ce2017ba897f
Jewel Rana <jewelranashepon@gmail.com>
2025-01-21 14:58:18 +0600

    Seed is updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
 import { db } from "../lib/db";
-<<<<<<< HEAD
 import { postdata } from "../app/data/postdata";
 
 async function main() {
@@ -44,48 +43,5 @@ async function main() {
     console.log("✅ Post seeding successful!");
   } else {
     console.log("✅ Posts already exist. Skipping seeding.");
-=======
-import bcrypt from "bcryptjs";
-import { userData, User } from "../app/data/userData";
-import { postdata, PostType } from "@/app/data/postdata";
-
-const users: Array<User> = Object.values(userData); // Convert object to array
-
-async function main() {
-  const userCount = await db.user.count();
-
-  if (!userCount) {
-    // Hash passwords for each user
-    const usersWithHashedPasswords = await Promise.all(
-      users.map(async (user) => ({
-        ...user,
-        password: await bcrypt.hash(user.password, 10), // Hash the password
-      }))
-    );
-
-    // Create users using `createMany`
-    await db.user.createMany({
-      data: usersWithHashedPasswords,
-      skipDuplicates: true,
-    });
-
-    console.log("User seeding was successful");
-  } else {
-    console.log("Users already exist. Skipping seeding.");
->>>>>>> b1e4e424bd0e39468c6a0737c6b009158e408717
   }
 }
-
-main()
-  .then(async () => {
-    await db.$disconnect();
-  })
-  .catch(async (error) => {
-<<<<<<< HEAD
-    console.error(" Error seeding database:", error);
-=======
-    console.error("Error seeding database:", error);
->>>>>>> b1e4e424bd0e39468c6a0737c6b009158e408717
-    await db.$disconnect();
-    process.exit(1);
-  });
